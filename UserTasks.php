45fdc1e20e547c76321da2ceedb6040741fa9c6d
Ruqayya <parveenruqayya341@gmail.com>
2024-04-09 20:50:59 +0530

    deleted all the files

--- UserTasks.php
+++
@@ -1,9 +0,0 @@
-<?php
-include("db_connect.php");
-// Fetch existing volunteer data
-$sqlFetchData = "SELECT * FROM `tasks` where `user_id`= '".$_GET["user_id"]."'";
-$result = $conn->query($sqlFetchData);
-
-// Close connection
-$conn->close();
-?>
--- userb.php
+++
@@ -1,6 +0,0 @@
-<?php
-// Include your database connection or any other necessary files
-include("db_connect.php");
-$sql = "SELECT * FROM `registration`";
-$result = $conn->query($sql);
-?>
