de8df8b23d093cb4782d21885cb2f27e96c91625
Ruqayya <parveenruqayya341@gmail.com>
2024-05-04 19:38:47 +0530

    added the code for admin login to redirect the admin to the home page

--- login.php
+++ login.php
@@ -4,6 +4,30 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $email = $_POST["email"];
     $password = $_POST["password"];
 
+//check if the admin is trying to login
+   $admin = "SELECT * FROM admin_details WHERE email='$email'";
+    $result = mysqli_query($conn, $admin);
+    if ($result && mysqli_num_rows($result) == 1) {
+        $row = mysqli_fetch_assoc($result);
+
+        if ($password == $row["password"]) {
+            // Password is correct, set session variables
+            $_SESSION["email"] = $row["email"];
+
+            // Set a cookie if "Remember Me" is checked
+            if (isset($_POST["remember"])) {
+                setcookie("email", $row["email"], time() + (86400 * 30), "/"); // 30 days
+            }
+
+           //redirect admin to home page
+            header("Location: ./Home_page.php");
+            exit();
+        } else {
+            // Invalid password
+            echo "Invalid password!";
+        }
+    } else {
+        
     // Query to fetch user data from the database
     $sql = "SELECT * FROM registration WHERE email='$email'";
     $result = mysqli_query($conn, $sql);
@@ -29,12 +53,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
             exit();
         } else {
             // Invalid password
+            header("Location: ./login.html?message=Invalid password!");
             echo "Invalid password!";
         }
     } else {
         // User not found
+        header("Location: ./login.html?message=User not found!");
         echo "User not found!";
     }
+}
 }
 mysqli_close($conn);
 
